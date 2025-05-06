538c0eaaed029479be28bf9da5f4d704a6a5c0cf
Semira <nyarkosemiratunatasha@gmail.com>
2025-05-06 01:34:59 +0000

    watchlist status

--- src/Components/WatchList.jsx
+++ src/Components/WatchList.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import '../Components/styles.css'
+import MovieCard from './MovieCard';
 
 const WatchList = ({ watchList, movies, toggleWatchlist}) => {
   return (
     <div>
       <h2 className='title'>Your WatchList</h2>
+      <div className="watchlist">
+        {
+          watchList.map(id =>{
+            const movie = movies.find(movie => movie.id === id);
+            return <MovieCard key={id} movie={movie} toggleWatchlist={toggleWatchlist} isWatchlisted={true}/>
+          })
+        }
+      </div>
     </div>
   )
 }
